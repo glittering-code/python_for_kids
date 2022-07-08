546de07835dc8ecd2e2259a1c1fa47392a58bbf0
glittering-code <glitteringcode9@gmail.com>
2022-07-08 11:22:54 +0530

    printing in multiple lines

---
+++ print/multiline.py
@@ -0,0 +1,7 @@
+print("I am vandana.")
+print("I love coding.")
+print("This is our first class of coding.")
+
+## another way of doing the same thing
+print("I am vandana.\nI love coding.\nThis is our first class of coding.")
+
